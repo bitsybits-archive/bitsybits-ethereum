b0b6fcfae0c5f5495c20efb5a8a4b97c1cee72d4
vldmkr <vladmakaryan@gmail.com>
2017-12-16 17:37:53 +0200

    ownable interface added, setting demo device implemented

--- contracts/SimpleDeviceMap.sol
+++ contracts/SimpleDeviceMap.sol
@@ -1,16 +1,29 @@
 pragma solidity ^0.4.18;
 
-contract SimpleDeviceMap {
+import './Ownable.sol';
+
+contract SimpleDeviceMap is Ownable {
     mapping(bytes8 => string) private commands;
     mapping(bytes8 => address) private owners;
+    bytes8 public demoDevice;
     
-    modifier onlyDeviceOwnerOrNew (bytes8 _device) {
+    function SimpleDeviceMap() public {
+        setDemoDevice("TEST");
+    }
+    
+    modifier onlyUsableDevices (bytes8 _device) {
         require(canUseDevice(_device));
         _;
     }
+    
+    function setDemoDevice (bytes8 _device) public
+    onlyOwner
+    {
+        demoDevice = _device;
+    }
 
     function setCommand (bytes8 _device, string _command) public 
-    onlyDeviceOwnerOrNew(_device)
+    onlyUsableDevices(_device)
     {
         commands[_device] = _command;
         owners[_device] = msg.sender;
@@ -25,6 +38,8 @@ contract SimpleDeviceMap {
     function canUseDevice (bytes8 _device) constant public
     returns (bool)
     {
-        return owners[_device] == msg.sender || owners[_device] == 0x0;
+        return demoDevice == _device
+        || owners[_device] == msg.sender
+        || owners[_device] == 0x0;
     }
 }
