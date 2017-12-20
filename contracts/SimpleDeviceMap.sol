67baa20a8bb79a9d94a02ea75555a1a43275cfe1
vldmkr <vladmakaryan@gmail.com>
2017-12-20 03:20:42 +0200

    strings lib added, libraries implemented

--- contracts/SimpleDeviceMap.sol
+++ contracts/SimpleDeviceMap.sol
@@ -1,11 +1,19 @@
 pragma solidity ^0.4.18;
 
 import './Ownable.sol';
+import './Strings.sol';
 
 contract SimpleDeviceMap is Ownable {
-    mapping(bytes8 => string) private commands;
-    mapping(bytes8 => address) private owners;
+    using Strings for *;
+    
     bytes8 public demoDevice;
+
+    mapping(bytes8 => string) private commands;
+    mapping(bytes16 => string) private libraries;
+    mapping(bytes8 => bytes16[]) private librariesUsedInCommand;
+
+    mapping(bytes8 => address) private deviceOwners;
+    mapping(bytes16 => address) private libraryOwners;
     
     function SimpleDeviceMap() public {
         setDemoDevice("TEST");
@@ -15,6 +23,11 @@ contract SimpleDeviceMap is Ownable {
         require(canUseDevice(_device));
         _;
     }
+
+    modifier onlyLibraryOwner (bytes16 _libname) {
+        require(canModifyLibrary(_libname));
+        _;
+    }
     
     function setDemoDevice (bytes8 _device) public
     onlyOwner
@@ -22,24 +35,50 @@ contract SimpleDeviceMap is Ownable {
         demoDevice = _device;
     }
 
-    function setCommand (bytes8 _device, string _command) public 
+    function setCommand (bytes8 _device, string _command, bytes16[] _libnames) public 
     onlyUsableDevices(_device)
     {
         commands[_device] = _command;
-        owners[_device] = msg.sender;
+        deviceOwners[_device] = msg.sender;
+        librariesUsedInCommand[_device] = _libnames;
     }
     
     function getCommand (bytes8 _device) constant public
     returns (string)
     {
-        return commands[_device];
+        string memory builtCommand = "";
+        bytes16[] memory libnames = librariesUsedInCommand[_device];
+        for (var i = 0; i < libnames.length; ++i) {
+            builtCommand = builtCommand.toSlice().concat(libraries[libnames[i]].toSlice());
+        }
+
+        return builtCommand.toSlice().concat(commands[_device].toSlice());
+    }
+
+    function setLibrary (bytes16 _libname, string _command) public 
+    onlyLibraryOwner(_libname)
+    {
+        libraries[_libname] = _command;
+        libraryOwners[_libname] = msg.sender;
+    }
+
+    function getLibrary (bytes16 _libname) constant public 
+    returns (string)
+    {
+        return libraries[_libname];
     }
     
     function canUseDevice (bytes8 _device) constant public
     returns (bool)
     {
         return demoDevice == _device
-        || owners[_device] == msg.sender
-        || owners[_device] == 0x0;
+        || deviceOwners[_device] == msg.sender
+        || deviceOwners[_device] == 0x0;
+    }
+
+    function canModifyLibrary (bytes16 _libname) constant public
+    returns (bool)
+    {
+        return libraryOwners[_libname] == 0x0; // || libraryOwners[_libname] == msg.sender;
     }
 }
